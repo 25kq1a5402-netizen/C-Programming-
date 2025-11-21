abce93f2a105c064afe5b01c1aef44a7db5a64dd
25kq1a5402-netizen <25kq1a5402@pace.ac.in>
2025-11-21 20:20:41 +0530

    Create Large.c

---
+++ Large.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main(){
+    int a, b, c;
+    printf("Enter three numbers \n");
+    scanf("%d%d%d",&a,&b,&c);
+    if(a>=b)
+      if(a>=c)
+          printf("a is large");
+      else
+          printf("c is large");
+    else
+       if("b>=c")
+          printf("b is large");
+       else
+          printf("c is large");
+}         
+   
