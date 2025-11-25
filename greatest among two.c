367fa66a40cc43c41dab740473172c6ce61a8aa3
25kq1a5402-netizen <25kq1a5402@pace.ac.in>
2025-11-25 22:11:15 +0530

    Create greatest among two.c

---
+++ greatest among two.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+    int a,b;
+    printf("enter a");
+    scanf("%d",&a);
+    printf("enter b");
+    scanf("%d",&b);
+    if(a>b)
+    printf("%d is greater",a);
+    else
+    printf("%d is greater",b);
+}
