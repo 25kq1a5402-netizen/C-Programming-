574c8333780cab405eaf41ec909458b3449c53dd
25kq1a5402-netizen <25kq1a5402@pace.ac.in>
2025-12-01 19:25:46 +0530

    Pointers ex.c

---
+++ Pointers ex.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+void main()
+{
+   int a=10;
+   int *b;
+   printf("%d",a);
+   printf("\n%d",&a);
+   b=&a;
+   printf("\n%d",b);
+   b=b+1;
+   printf("\n%d",b);
+   b++;
+   printf("\n%d",b);
+}   
+     
+                     
+         
+          
+                      
+          
+  
+
+
+
+    
+          
+          
+          
+          
+     
+
+    
+             
+          
+         
