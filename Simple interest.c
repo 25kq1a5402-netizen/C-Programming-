d9782b132233047dbee93c7c6beca3aab07e4531
25kq1a5402-netizen <25kq1a5402@pace.ac.in>
2025-12-07 13:11:11 +0530

    Simple interest.c

---
+++ Simple interest.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+void main()
+{
+     float principle,rate,time,simple_interest;
+     clrscr();
+     printf("Enter the principle:");
+     scanf("%f",&principle);
+     printf("Enter the rate:");
+     scanf("%f",&rate);
+     printf("Enter the time:");
+     scanf("%f",&time);
+     simple_interest=principle*rate*time/100;
+     printf("Simple interest is%f",simple_interest);
+     getch();
+}     
+
+
+
+
+
+    
+          
+          
+          
+          
+     
+
+    
+             
+          
+         
