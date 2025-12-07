990d8d195743d0f2bad77eaf42786651d20f0914
25kq1a5402-netizen <25kq1a5402@pace.ac.in>
2025-12-07 12:59:04 +0530

    Squarerootofanum.c

---
+++ Squarerootofanum.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+void main()
+{
+     int x;
+     double res;
+     clrscr();
+     printf("\n Enter any number to get the square root:");
+     scanf("%d",&x);
+     res=sqrt(x);
+     printf("The square root of %d is:%.lf",x,res);
+     getch();
+}     
+
+
+
+
+
+    
+          
+          
+          
+          
+     
+
+    
+             
+          
+         
