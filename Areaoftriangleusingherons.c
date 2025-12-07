a1baf6d9a98d00c602bd5865e489110a5880e22c
25kq1a5402-netizen <25kq1a5402@pace.ac.in>
2025-12-07 19:02:56 +0530

    Areaoftriangleusingherons.c

---
+++ Areaoftriangleusingherons.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<math.h>
+void main()
+{
+    float sideOne,sideTwo,sideThree,s,area;
+    printf("Enter the length of three sides of triangle\n");
+    scanf("%f%f%f",&sideOne,&sideTwo,&sideThree);
+    s=(sideOne+sideTwo+sideThree)/2;
+    area=sqrt(s*(s-sideOne)*(s-sideTwo)*(s-sideThree));
+    printf("Area of triangle:%f",area);
+}    
+
+
+
+
+
+    
+          
+          
+          
+          
+     
+
+    
+             
+          
+         
