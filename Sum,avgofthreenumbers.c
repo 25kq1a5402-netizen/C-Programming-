590045439a01dad29900da6f062342754e1c0e92
25kq1a5402-netizen <25kq1a5402@pace.ac.in>
2025-12-07 13:20:45 +0530

    Sum,avgofthreenumbers.c

---
+++ Sum,avgofthreenumbers.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+     int num1,num2,num3,sum;
+     float avg;
+     clrscr();
+     printf("Enter the First Number = ");
+     scanf("%d",&num1);
+     printf("Enter the Second Number = ");
+     scanf("%d",&num2);
+     printf("Enter the Third Number  ");
+     scanf("%d",&num3);
+     avg=sum/3;
+     printf("\n The Sum of Three Numbers=%d",sum);
+     printf("\n The Average of Three Numbers=%f",avg);
+     getch();
+}     
+
+
+
+
+
+    
+          
+          
+          
+          
+     
+
+    
+             
+          
+         
