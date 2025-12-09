59c94f981c7433c5bc91125c06831cc6280b9eab
25kq1a5402-netizen <25kq1a5402@pace.ac.in>
2025-12-09 22:57:20 +0530

    Realloc.c

---
+++ Realloc.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<conio.h>
+void main()
+{
+    int *ptr=(int*)malloc(3*sizeof(int));
+    clrscr();
+    ptr[0]=1;
+    ptr[1]=2;
+    ptr[2]=3;
+    ptr=(int*)realloc(ptr,5*sizeof(int));
+    ptr[3]=4;
+    ptr[4]=5;
+    for(int i=0;i<5;i++)
+    {
+        printf("%d",ptr[i]);
+     }
+        free(ptr);
+        getch();
+     }      
+
+
+    
+          
+          
+          
+          
+     
+
+    
+             
+          
+         
