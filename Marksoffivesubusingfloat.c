f636d5ecf2c4eb0328c7738c2ef624bf0ec43159
25kq1a5402-netizen <25kq1a5402@pace.ac.in>
2025-12-16 18:45:42 +0530

    Marksoffivesubusingfloat.c

---
+++ Marksoffivesubusingfloat.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<conio.h>
+#include<conio.h>
+void main()
+{
+  int eng,mat,phy,chem,comp;
+  float sum,avg;
+  clrscr();
+  printf("enter 5 subjects marks:\n");
+  scanf("%d%d%d%d%d",&eng,&mat,&phy,&chem,&comp);
+  sum=eng+mat+phy+chem+comp;
+  avg=sum/5;
+  printf("sum=%f\n",sum);
+  printf("avg=%f",avg);
+  getch();
+}  
+         
+    
+
+             
+          
+          
+          
+          
+     
+
+    
+             
+          
+         
