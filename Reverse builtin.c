83891208d41143783b7c48f47ac1bf96dba6f054
25kq1a5402-netizen <25kq1a5402@pace.ac.in>
2025-12-20 19:53:55 +0530

    Reverse builtin.c

---
+++ Reverse builtin.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+char a[50];
+clrscr();
+printf("enter a string");
+gets(a);
+strrev(a);
+printf("reversed string=%s",a);
+getch();
+}
+
+  
+    
+
+             
+          
+          
+          
+          
+     
+
+    
+             
+          
+         
